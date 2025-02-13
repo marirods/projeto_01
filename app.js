5d609fa7ed577fb8869266f4db94719c019b4dd5
marirods <mariane.rodrigues933@gmail.com>
2025-02-13 17:24:44 -0300

    em andamento

---
+++ app.js
@@ -0,0 +1,15 @@
+/*******************************************************************************************************************
+ * Objetivo : API referente ao projeto de controle de jogos
+ * Data: 13/02/2025
+ * Autor: Marcel
+ * Versão: 1.0
+ * Observação: 
+ ************** Para configurar e instalar a API, precisamos das seguintes bibliotecas:
+*                             express          npm install express --save
+*                             cors             npm install cors --save
+*                              body-parser      npm install body-parser --save 
+*                              
+************* Para configurar e Instalar o acesso ao Banco de Dados precisamos:
+*                    prisma           npm install prisma --save (conexão com o BD)
+*                    prisma/client    npm install @prisma/client --save (Executa scripts no BD)
+*****************************************************************************************************************/
---
+++ model/DAO/jogo.js
@@ -0,0 +1,71 @@
+/*******************************************************************************************************************
+ * Objetivo : Model responsável pelo CRUD de dados referente a jogos no Banco de Dados
+ * Data: 13/02/2025
+ * Autor: Marcel
+ * Versão: 1.0
+ * ********************************/
+
+ //Import da biblioteca do prisma client para executar scripts no BD
+ const { PrismaClient } = require('@prisma/client')
+
+
+ 
+ //Função para inserir no Banco de Dados um novo jogo
+ const insertJogo = async function(jogo){
+     //Instancia da classe do prisma client, para gerar um objeto
+     const prisma = new PrismaClient()
+
+     let sql = `insert into tbl_jogo(
+                                        nome,
+                                        data_lancamento,
+                                        versao,
+                                        tamanho, 
+                                        descricao,
+                                        foto_capa,
+                                        link
+                                    ) values (
+                                        ${jogo.nome},
+                                        ${jogo.data_lancamento},
+                                        ${jogo.versao},
+                                        ${jogo.tamanho},
+                                        ${jogo.descricao},
+                                        ${jogo.foto_capa},
+                                        ${jogo.link}
+                                    )`
+
+    //Executa o script SQL no BD e AGUARDA o retorno do BD
+    let result = await prisma.$executeRawUnSafe(sql)
+
+    if (result)
+        return true
+    else
+        return false
+ }
+
+//Função para atualizar no Banco de Dados um jogo existente
+ const updateJogo = async function(){
+
+}
+
+//Função para excluir no Banco de Dados um jogo existente
+const deleteJogo = async function(){
+
+}
+
+//Função para retornar do Banco de Dados uma lista de jogos
+const selectAllJogo = async function(){
+
+}
+
+//Função para buscar no Banco de Dados um jogo pelo ID
+const selectByIdJogo = async function(){
+
+}
+
+module.exports = {
+    insertJogo,
+    updateJogo,
+    deleteJogo,
+    selectAllJogo,
+    selectByIdJogo
+}
